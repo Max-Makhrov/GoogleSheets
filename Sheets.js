83059807d505f4b7ae97166ded298d5c0e828d6a
Max Makhrov <makhrov.max@gmail.com>
2017-03-01 14:19:50 +0200

    Update Sheets.js

--- Sheets.js
+++ Sheets.js
@@ -16,3 +16,22 @@ function copySheet(SS, strToCopy, strName) {
   SS.moveActiveSheet(index);
 
 }
+
+/*
+  Creates sheet if it does not exist
+  Returns sheet object
+ 
+  * ss           SpreadSheet                 default: current sheet
+  * name         Name of sheet
+*/
+function createSheetIfNotExists(ss, name) { 
+  var ss = ss || SpreadsheetApp.getActiveSpreadsheet();
+  
+  try {ss.setActiveSheet(ss.getSheetByName(name));}
+    catch (e) {ss.insertSheet(name);} 
+  
+  var sheet = ss.getSheetByName(name);
+  return sheet;
+}
+
+
