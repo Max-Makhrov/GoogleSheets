2af4fbb46c10b4e9e5e198d18f7389ae792270f5
Max Makhrov <makhrov.max@gmail.com>
2017-10-21 15:41:16 +0300

    Update onEdit.js

--- onEdit.js
+++ onEdit.js
@@ -1,3 +1,72 @@
+
+function ObjectOnEdit(e)
+{
+  var self = this;
+  
+  // range
+  var r = e.range;
+  // var r = SpreadsheetApp.getActive().getActiveRange();
+  var numCols = r.getWidth();
+  var numRows = r.getHeight();
+  var boolCell = true;
+  if (numRows > 1 || numCols > 1) { boolCell = false; }
+  var numRow = r.getRow();
+  var numCol = r.getColumn();
+  var columns = [];
+  if (boolCell) { columns.push(numCol); }
+  else
+  {
+   for (var i = numCol; i <= numCol + numCols - 1; i++)
+   {
+    columns.push(i);      
+   }
+  }
+  
+  self.range = r;
+  self.numCols = numCols;
+  self.numRows = numRows;
+  self.numRow = numRow;
+  self.numCol = numCol;
+  self.boolCell = boolCell;
+  self.columns = columns;
+  
+  
+  // get value or values
+  var value = e.value;
+  var values = [];
+  if (value == undefined) { value = ''; }
+  if (typeof value == 'object') { value = ''; }
+  if (boolCell && value == '') { value = r.getValue(); }
+  if (!boolCell && value == '') { values = r.getValues(); }
+  
+  self.value = value;
+  self.values = values;
+  
+  
+  // old value
+  var oldValue = e.oldValue;
+  if (oldValue == undefined) { oldValue = ''; }
+  if (typeof oldValue == 'object') { oldValue = ''; }
+  
+  self.oldValue = oldValue;
+  
+  // sheet and file
+  var sheet = r.getSheet();
+  var file = sheet.getParent();
+  var idFile = file.getId();
+  var nameSheet = sheet.getName();
+  var idSheet = sheet.getSheetId(); 
+  
+  self.sheet = sheet;
+  self.file = file;
+  self.idFile = idFile;
+  self.nameSheet = nameSheet;
+  self.idSheet = idSheet;
+  
+}
+
+
+
 /*----------------------------------- Gets -------------------------------------------------*/
 function getDvValue(e) {
   var value = e.value;
