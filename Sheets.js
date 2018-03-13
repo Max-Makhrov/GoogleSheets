9e9a3ad41e2bad288901805ca8c0f429152312bc
Max Makhrov <makhrov.max@gmail.com>
2018-03-13 16:26:00 +0200

    Update Sheets.js
    added function deleteEmptyCells.
    
    It deletes empty rows and columns from all sheets. Leave 25 rows and 1 column as a reserve.

--- Sheets.js
+++ Sheets.js
@@ -262,3 +262,41 @@ function createSheetIfNotExists(ss, name) {
 }
 
 
+// ************************************************************************************************************
+var C_COLS_LEAVE = 1;
+var C_ROWS_LEAVE = 25;
+
+
+function deleteEmptyCells() {
+  var file = SpreadsheetApp.getActive();
+  var sheets = file.getSheets();  
+  for (var i = 0, l = sheets.length; i < l; i++) { deleteEmptyCellsSheet_(sheets[i]); }
+  return 0;  
+  
+}
+
+function deleteEmptyCellsSheet_(sheet)
+{
+  // columns
+  var cLast = sheet.getLastColumn();
+  var cEnd = sheet.getMaxColumns();
+  if ( (cEnd - cLast) > C_COLS_LEAVE ) 
+  {
+    var numC = cEnd - cLast - C_COLS_LEAVE;
+    sheet.deleteColumns(cLast + 1, numC);  
+  }
+  
+  // rows
+  var rLast = sheet.getLastRow();
+  var rEnd = sheet.getMaxRows();
+  if ( (rEnd - rLast) > C_ROWS_LEAVE ) 
+  {
+    var numC = rEnd - rLast - C_ROWS_LEAVE;
+    sheet.deleteRows(rLast + 1, numC);  
+  }  
+}
+
+// ************************************************************************************************************
+
+
+
