07fe7ca0c3fb5254c716d4b25651b5225afdf378
Max Makhrov <makhrov.max@gmail.com>
2018-10-19 17:43:04 +0300

    Update Triggers.js

--- Triggers.js
+++ Triggers.js
@@ -43,3 +43,47 @@ function checkTriggerExists(nameFunction, triggerSourceType)
   return false; 
 
 }
+
+
+
+// UPD 201810
+function setTriggerOnHour_(functionName)
+{
+  if (isTriggerHere_(functionName, 'CLOCK', 'CLOCK')) { return -1; } // trigger already exists
+  var file = SpreadsheetApp.getActive();
+  ScriptApp.newTrigger(functionName)
+    .timeBased()
+    .everyHours(1)
+    .create();
+  return 0;
+}
+
+
+
+function isTriggerHere_(functionName, eventType, triggerSource)
+{
+  var triggers = ScriptApp.getProjectTriggers();
+  var trigger = {};  
+  for (var i = 0; i < triggers.length; i++) {
+    trigger = triggers[i];
+    if (trigger.getHandlerFunction() == functionName && trigger.getTriggerSource() == triggerSource && trigger.getEventType() == triggerSource) return true;
+  }  
+  return false;
+}
+
+
+
+function deleteTriggerOnHour_(functionName)
+{
+  var triggers = ScriptApp.getProjectTriggers();
+  var trigger = {};  
+  for (var i = 0; i < triggers.length; i++) {
+    trigger = triggers[i];
+    if (trigger.getHandlerFunction() == functionName && trigger.getTriggerSource() == 'CLOCK' && trigger.getEventType() == 'CLOCK')
+    {
+      ScriptApp.deleteTrigger(trigger);
+      return 0;
+    }
+  }  
+  return -1; // trigger is not founf  
+}
